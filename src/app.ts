d8edfad851efe433884b0b705c744bf3ba3559af
Michal Hromas <michalhromas1@gmail.com>
2021-01-16 22:01:23 +0100

    Fetch products

--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,41 @@ export const app = {
     const url = 'https://rohlik.cz';
     const inputSelector = '#searchGlobal';
     const submitSelector = '#searchForm button';
-    const query = 'losos';
+    const query = 'jahody';
 
     await pup.navigateTo(page, url);
     await pup.search(page, inputSelector, submitSelector, query);
-    await page.waitForTimeout(10000);
+
+    await this.listProducts(page);
+  },
+
+  async listProducts(page: puppeteer.Page): Promise<void> {
+    await page.waitForSelector('.productCard__title');
+    const products = await page.evaluate(() => {
+      const productEls = document.querySelectorAll('.productCard__wrapper');
+      // const pps: any[] = [];
+      // productEls.forEach((p) => {
+      //   pps.push({
+      //     title: p.querySelector('.productCard__title')?.textContent,
+      //     price: p.querySelector('.cardPrice')?.textContent,
+      //     pricePerKg: p.querySelector('.pricePerOffer')?.textContent,
+      //     quantity: p.querySelector('.quantity')?.textContent,
+      //   });
+      // });
+
+      const ps = Array.from(productEls).map((p) => ({
+        title: p.querySelector('.productCard__title')?.textContent?.trim(),
+        price: `${p
+          .querySelector('.cardPrice .wrap .price')
+          ?.textContent?.trim()}.${p
+          .querySelector('.cardPrice .wrap .fraction')
+          ?.textContent?.trim()}`,
+        pricePerKg: p.querySelector('.pricePerOffer')?.textContent?.trim(),
+        quantity: p.querySelector('.quantity')?.textContent?.trim(),
+      }));
+
+      return ps;
+    });
+    console.log(products);
   },
 };
