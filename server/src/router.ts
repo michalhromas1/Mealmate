142e07206e16d4c75b21c701ccaa5021c01db1d7
Michal Hromas <michalhromas1@gmail.com>
2021-01-26 17:48:19 +0100

    refactor

--- server/src/router.ts
+++ server/src/router.ts
@@ -10,16 +10,47 @@ export const createAppRouter = (): Router => {
   const router = new AppRouter();
 
   router.addGetRoute('products', async (res) => {
-    console.log('running benchmark...');
+    // console.log('running benchmark...');
 
-    let totalTime = 0;
-    const nOfTries = 1;
+    // let totalTime = 0;
+    // const nOfTries = 1;
+
+    // let fetchedProducts: Product[] = [];
+
+    // for (let i = 0; i < nOfTries; i++) {
+    //   const t0 = Date.now();
+
+    //   const eshops: Eshop[] = [rohlik, kosik];
+    //   const queries = [
+    //     'mango',
+    //     'losos',
+    //     'chřest',
+    //     // 'banán',
+    //     // 'mrkev',
+    //     // 'palačinky',
+    //     // 'pomelo',
+    //     // 'kuře',
+    //     // 'těstoviny',
+    //     // 'bramborový salát',
+    //   ];
+
+    //   const products = new Products(eshops, queries);
+    //   fetchedProducts = await products.fetchProducts();
+
+    //   const time = Date.now() - t0;
+    //   totalTime += time;
+
+    //   console.log(Date.now() - t0, 'ms');
+    // }
+
+    // console.log(`avg: ${totalTime / nOfTries} ms`);
+    // console.log('benchmark finished');
+
+    // res.json(fetchedProducts);
 
     let fetchedProducts: Product[] = [];
 
-    for (let i = 0; i < nOfTries; i++) {
-      const t0 = Date.now();
-
+    await measure(5, async () => {
       const eshops: Eshop[] = [rohlik, kosik];
       const queries = [
         'mango',
@@ -33,21 +64,30 @@ export const createAppRouter = (): Router => {
         // 'těstoviny',
         // 'bramborový salát',
       ];
-
       const products = new Products(eshops, queries);
       fetchedProducts = await products.fetchProducts();
-
-      const time = Date.now() - t0;
-      totalTime += time;
-
-      console.log(Date.now() - t0, 'ms');
-    }
-
-    console.log(`avg: ${totalTime / nOfTries} ms`);
-    console.log('benchmark finished');
+    });
 
     res.json(fetchedProducts);
   });
 
   return router.getRouter();
 };
+
+async function measure(tryCount: number, subject: () => any): Promise<void> {
+  let totalTime = 0;
+
+  console.log('running benchmark...');
+
+  for (let i = 0; i < tryCount; i++) {
+    const t0 = Date.now();
+    await subject();
+    const time = Date.now() - t0;
+    totalTime += time;
+
+    console.log(Date.now() - t0, 'ms');
+  }
+
+  console.log(`avg: ${totalTime / tryCount} ms`);
+  console.log('benchmark finished');
+}
